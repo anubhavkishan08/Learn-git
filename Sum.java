c097c149bdd2a116d971b0036b439aa63a714903
Anubhav Kishan <anubhavkishan08@gmail.com>
2022-07-16 17:22:34 +0530

    commit all

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String[] args){
         int a=5,b=6;
         int sum=a+b;
-        System.out.pintln(sum);
+        System.out.println("The sum is"+sum);
     }
 }
