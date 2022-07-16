59c754269a5ff2a0ebee0f6ae8d22fac90533665
Anubhav Kishan <anubhavkishan08@gmail.com>
2022-07-16 17:30:30 +0530

    commit all multiply

---
+++ Diff.java
@@ -0,0 +1,7 @@
+public class Diff {
+    public static void main(String[] args){
+        int a=5,b=6;
+        int diff=a-b;
+        System.out.println("The sum is"+diff);
+    }
+}
---
+++ Mult.java
@@ -0,0 +1,7 @@
+public class Mult {
+    public static void main(String[] args){
+        int a=5,b=6;
+        int sum=a*b;
+        System.out.println("The ans is"+sum);
+    }
+}
